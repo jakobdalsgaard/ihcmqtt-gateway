047e04d98766160c7f621bdb7bc1f7edbcf74e2c
Jakob Dalsgaard <jakob@dalsgaard.net>
2022-01-05 12:56:44 +0100

    Release version 1.0.1

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ README = (HERE / "README.md").read_text()
 # This call to setup() does all the work
 setup(
     name="ihcmqtt-gateway",
-    version="1.0.0",
+    version="1.0.1",
     description="IHC-MQTT gateway",
     long_description=README,
     long_description_content_type="text/markdown",
